1a93750ac98b6aca18d008690dd6979309bd6a76
Stéphane Hordoir <stephane.hordoir@gmail.com>
2024-01-17 14:37:06 +0100

    remove old

--- next/snippets/src/app/snippets/[id]/loading.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { LayoutProps } from "../../../../.next/types/app/layout";
-
-export default function SnippetShowLoadingPage() {
-  return <div>Loading...</div>;
-}
